48cd74788377f91eb5c99b9e97c40b51d4ea273c
Raphael Jairos <47027274+jairos75@users.noreply.github.com>
2023-11-09 00:12:54 +0300

    step-4

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
+import 'dart:developer' as devtools show log;
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -64,16 +65,20 @@ class _LoginViewState extends State<LoginView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                final userCredential = await FirebaseAuth.instance
-                    .signInWithEmailAndPassword(
-                        email: email, password: password);
-                print(userCredential);
+                await FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                  '/notes/',
+                  (route) => false,
+                );
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'INVALID_LOGIN_CREDENTIALS') {
-                  print('User not found');
+                  devtools.log('User not found');
                 } else {
-                  print('SOMETHING ELSE HAPPENED');
-                  print(e.code);
+                  devtools.log('SOMETHING ELSE HAPPENED');
+                  devtools.log(e.code.toString());
                 }
               }
             },
