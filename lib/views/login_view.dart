3038bfb7a49e5aa2b1a6811e3d40f792d01a27b4
Raphael Jairos <47027274+jairos75@users.noreply.github.com>
2023-11-10 19:52:05 +0300

    step-7

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/firebase_options.dart';
-
 import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
